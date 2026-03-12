37543199c87f5118672428934e47c61918ae8095
Arunoda Susiripal <arunoda.susiripala@gmail.com>
2026-03-12 14:20:31 +0530

    Improve the look

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,16 @@
+import Head from "next/head";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link
+          href="https://fonts.googleapis.com/css2?family=VT323&family=IBM+Plex+Mono:ital,wght@0,400;0,500;0,600;1,400&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
